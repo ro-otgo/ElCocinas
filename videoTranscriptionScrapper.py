297057ff700ca56fe5176e18f1a89dc3ba371f54
ro-otgo <116685946+ro-otgo@users.noreply.github.com>
2022-11-24 19:56:40 +0100

    Añadir carpeta para almacenar videos

--- videoTranscriptionScrapper.py
+++ videoTranscriptionScrapper.py
@@ -1,13 +1,19 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 import json
+import os
 
-def downloadVideoScript(videoId:str):
+def create_transcript_folder(transcript_folder:str):
+    if not os.path.exists(transcript_folder):
+        os.mkdir(transcript_folder)
+
+def downloadVideoScript(videoId:str, transcript_folder='transcript'):
     transcript_list = YouTubeTranscriptApi.list_transcripts(videoId)
     transcript = transcript_list.find_transcript(['en'])
     data_transcript = transcript.fetch()
-    with open('video_{}.json'.format(videoId), 'w', encoding='utf-8') as file:
+    with open(os.path.join(transcript_folder,'video_{}.json'.format(videoId)), 'w', encoding='utf-8') as file:
         file.write(json.dumps(data_transcript,indent=2))
 
 
 if __name__ == "__main__":
+    create_transcript_folder('transcript')
     downloadVideoScript('Ge7c7otG2mk')
