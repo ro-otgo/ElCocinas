ff02d8d0381cab322aa2179b8a21724bc7d2d171
ro-otgo <116685946+ro-otgo@users.noreply.github.com>
2022-11-24 20:20:59 +0100

    Anadir lista videos

--- videoTranscriptionScrapper.py
+++ videoTranscriptionScrapper.py
@@ -1,19 +1,29 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 import json
 import os
+import re
 
-def create_transcript_folder(transcript_folder:str):
+def create_transcript_folder(transcript_folder:str) -> None:
     if not os.path.exists(transcript_folder):
         os.mkdir(transcript_folder)
 
-def downloadVideoScript(videoId:str, transcript_folder='transcript'):
+def downloadVideoScript(videoId:str, transcript_folder='transcript') -> None:
     transcript_list = YouTubeTranscriptApi.list_transcripts(videoId)
     transcript = transcript_list.find_transcript(['en'])
     data_transcript = transcript.fetch()
     with open(os.path.join(transcript_folder,'video_{}.json'.format(videoId)), 'w', encoding='utf-8') as file:
         file.write(json.dumps(data_transcript,indent=2))
 
+def load_youtube_files(file_path:str) -> list:
+    videos_id_list = []
+    with open(file_path,'r') as file:
+        for l in file.readlines():
+            video_id = re.findall('.+youtube.+watch\?v=(\w+)',l)
+            videos_id_list.extend(video_id)
+    return videos_id_list
 
 if __name__ == "__main__":
     create_transcript_folder('transcript')
-    downloadVideoScript('Ge7c7otG2mk')
+    lista_videos = load_youtube_files('source_videos.txt')
+    for v in lista_videos:
+        downloadVideoScript(v)
