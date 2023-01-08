6299f532ef0b6b893e2f9fcb3ae65f63649046e4
Chus <22170934@live.uem.es>
2023-01-08 23:52:11 +0100

    URL_Primeros_Meet

--- playlist_scrapper.py
+++ playlist_scrapper.py
@@ -4,8 +4,8 @@ from pytube import Playlist
 
 
 if __name__ == "__main__":
-    playlist_url = 'https://www.youtube.com/watch?v=VdWM48aTiXA&list=PL2LHabI5sYyEyyuDesusF4FwGXjePVz37'
+    playlist_url = 'https://www.youtube.com/playlist?list=PLnujfCpADfgcntTD26TzIz9gbol97JfUa'
     p = Playlist(playlist_url)
     videos_url = p.video_urls
-    with open('playlist_PL2LHabI5sYyEyyuDesusF4FwGXjePVz37.txt','w') as file:
+    with open('playlist?list=PLnujfCpADfgcntTD26TzIz9gbol97JfUa.txt','w') as file:
         file.writelines('\n'.join(videos_url))
